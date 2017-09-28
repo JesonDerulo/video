066ec29bee99e66bfea2036a8f5681a6611748d5
CJ <cj@CJs-MacBook-Pro.local>
2017-09-27 17:20:39 -0700

    30- Migrate Courses App

---
+++ courses/views.py
@@ -0,0 +1,65 @@
+import random
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import (
+        CreateView,
+        DetailView,
+        ListView,
+        UpdateView,
+        DeleteView
+    )
+
+
+from videos.mixins import MemberRequiredMixin, StaffMemberRequiredMixin
+from .models import Course
+from .forms import CourseForm
+
+
+class CourseCreateView(StaffMemberRequiredMixin, CreateView):
+    model = Course
+    form_class = CourseForm
+    # success_url = "/success/"
+
+    def form_valid(self, form):
+        obj = form.save(commit=False)
+        obj.user = self.request.user
+        obj.save()
+        return super(CourseCreateView, self).form_valid(form)
+    
+
+
+class CourseDetailView(MemberRequiredMixin, DetailView):
+    queryset = Course.objects.all()
+
+
+class CourseListView(ListView):
+    def get_queryset(self):
+        request = self.request
+        qs = Course.objects.all()
+        query = request.GET.get('q')
+        if query:
+            qs = qs.filter(title__icontains=query)
+        return qs  #.filter(title__icontains='vid') #.filter(user=self.request.user)
+
+    # def get_context_data(self, *args, **kwargs):
+    #     context = super(VideoListView, self).get_context_data(*args, **kwargs)
+    #     context['random_number'] = random.randint(100, 10000)
+    #     print(context)
+    #     return context
+
+
+class CourseUpdateView(StaffMemberRequiredMixin, UpdateView):
+    queryset = Course.objects.all()
+    form_class = CourseForm
+    
+    def form_valid(self, form):
+        obj = form.save(commit=False)
+        if not self.request.user.is_staff:
+            obj.user = self.request.user
+        obj.save()
+        return super(CourseUpdateView, self).form_valid(form)
+
+
+class CourseDeleteView(StaffMemberRequiredMixin, DeleteView):
+    queryset = Course.objects.all()
+    success_url = '/videos/'
--- src/urls.py
+++ src/urls.py
@@ -1,38 +1,23 @@
-"""src URL Configuration
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.10/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.conf.urls import url, include
-    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
-"""
-from django.conf.urls import url
+from django.conf import settings
+from django.conf.urls.static import static
+from django.conf.urls import url, include
 from django.contrib import admin
 
-from videos.views import (
-    VideoListView,
-    VideoDetailView,
-    VideoCreateView,
-    VideoUpdateView,
-    VideoDeleteView,
-)
+
+
 from .views import home, HomeView
+
 
 urlpatterns = [
     url(r'^$', HomeView.as_view(), name='home'),
     url(r'^admin/', admin.site.urls),
-    url(r'^videos/$', VideoListView.as_view(), name='video-list'),
-    url(r'^videos/create/$', VideoCreateView.as_view(),name='video-create'),
-    # url(r'^videos/(?P<pk>\d+)$', VideoDetailView.as_view(), name='video-detail'),
-    url(r'^videos/(?P<slug>[\w-]+)/$', VideoDetailView.as_view(), name='video-detail-slug'),
-    url(r'^videos/(?P<slug>[\w-]+)/edit/$', VideoUpdateView.as_view(), name='video-update'),
-    url(r'^videos/(?P<slug>[\w-]+)/delete/$', VideoDeleteView.as_view(), name='video-delete'),
-
+    url(r'^courses/', include('courses.urls', namespace='courses')),
+    url(r'^videos/', include('videos.urls', namespace='videos')),
 ]
+
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
