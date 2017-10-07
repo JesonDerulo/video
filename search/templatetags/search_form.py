ead814dd109b247c4c77380f5895886db22855e9
CJ <cj@CJs-MacBook-Pro.local>
2017-10-07 08:23:35 -0700

    66- Search Template Tag

---
+++ search/templatetags/search_form.py
@@ -0,0 +1,7 @@
+# coding=utf-8
+from django import template
+register = template.Library()
+
+@register.inclusion_tag('search/snippets/search_form.html')
+def search_form(request):
+    return {"request": request}
